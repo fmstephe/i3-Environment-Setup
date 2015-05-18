7aee3d6720290dce74bcd9a5f9ec06694a2bbe83
Francis Stephens <francisstephens@gmail.com>
2015-05-18 15:29:12 +0100

    Removed guest additions installations

--- all.sh
+++ all.sh
@@ -4,8 +4,3 @@ sudo apt-get --assume-yes install xorg i3 gnome-terminal tree
 cp -r .i3/ ../
 cp -r .gconf/ ../
 cp .bashrc ../
-sudo apt-get --assume-yes install build-essential module-assistant
-sudo mount /dev/sr0 /media/cdrom/
-cd /media/cdrom/
-sh ./VBoxLinuxAdditions.run
-echo "Restart to complete installation"
