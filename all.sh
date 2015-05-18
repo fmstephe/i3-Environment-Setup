2ed751401dc95f122107458494476ffb741753cf
Francis Stephens <francisstephens@gmail.com>
2015-05-18 15:56:13 +0100

    Installing zip

--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get --assume-yes install xorg i3 gnome-terminal tree
+sudo apt-get --assume-yes install xorg i3 gnome-terminal tree zip
 cp -r .i3/ ../
 cp -r .gconf/ ../
 cp .bashrc ../
