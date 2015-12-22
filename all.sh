809d925c73f9e4921d807f200cdf814ce32d0d1d
Francis Stephens <francisstephens@gmail.com>
2015-12-22 16:17:39 +0000

    Adding htop to installed apps

--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get --assume-yes install xorg i3 gnome-terminal tree zip
+sudo apt-get --assume-yes install xorg i3 gnome-terminal tree zip htop
 cp -r .i3/ ../
 cp -r .gconf/ ../
 cp .bashrc ../
